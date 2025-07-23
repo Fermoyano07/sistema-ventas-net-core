eca8003d544bbdd03c51d931fc7b8d1c2b12ac5b
Fernando Moyano <fermoyano07@gmail.com>
2025-07-23 12:30:16 -0300

    Agregar archivos de proyecto.

---
+++ SistemaVenta.IOC/Dependencia.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+using SistemaVenta.DAL.DbContext;
+//using SistemaVenta.DAL.Interfaces;
+//using SistemaVenta.DAL.Implementacion;
+//using SistemaVenta.BLL.Interfaces;
+//using SistemaVenta.BLL.Implementacion;
+
+namespace SistemaVenta.IOC
+{
+    public static class Dependencia
+    {
+        //Cadena de Conexión
+        public static void InyectarDependencia(this IServiceCollection services, IConfiguration Configuration) {
+            services.AddDbContext<DbventaContext>(options =>
+            {
+                options.UseSqlServer(Configuration.GetConnectionString("CadenaSQL"));
+            });
+        }
+
+
+    }
+}
