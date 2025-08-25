4ab3038e300bead4b6362f9b76f60a3294fa0f1b
Fernando Moyano <fermoyano07@gmail.com>
2025-08-25 16:40:13 -0300

    20 / Implementación de solicitudes https para el módulo de ventas.

--- SistemaVenta.AplicacionWeb/Controllers/VentaController.cs
+++ SistemaVenta.AplicacionWeb/Controllers/VentaController.cs
@@ -1,9 +1,27 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using AutoMapper;
+using SistemaVenta.AplicacionWeb.Models.ViewModels;
+using SistemaVenta.AplicacionWeb.Utilidades.Response;
+using SistemaVenta.BLL.Interfaces;
+using SistemaVenta.Entity;
+using SistemaVenta.Entity.Models;
 
 namespace SistemaVenta.AplicacionWeb.Controllers
 {
     public class VentaController : Controller
     {
+        private readonly ITipoDocumentoVentaService _tipoDocumentoVentaService;
+        private readonly IVentaService _ventaService;
+        private readonly IMapper _mapper;
+
+        public VentaController(ITipoDocumentoVentaService tipoDocumentoVentaService,
+            IVentaService ventaService,
+            IMapper mapper)
+        {
+            _tipoDocumentoVentaService = tipoDocumentoVentaService;
+            _ventaService = ventaService;
+            _mapper = mapper;
+        }
         public IActionResult NuevaVenta()
         {
             return View();
@@ -12,6 +30,53 @@ namespace SistemaVenta.AplicacionWeb.Controllers
         public IActionResult HistorialVenta()
         {
             return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> ListaTipoDocumentoVenta() 
+        {
+            List<VMTipoDocumentoVenta> vmListaTipoDocumentos = _mapper.Map<List<VMTipoDocumentoVenta>>(await _tipoDocumentoVentaService.Lista());
+            return StatusCode(StatusCodes.Status200OK, vmListaTipoDocumentos);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> ObtenerProductos(string busqueda)
+        {
+            List<VMProducto> vmListaProductos = _mapper.Map<List<VMProducto>>(await _ventaService.ObtenerProductos(busqueda));
+            return StatusCode(StatusCodes.Status200OK, vmListaProductos);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> RegistrarVenta([FromBody] VMVenta modelo)
+        {
+            GenericResponse<VMVenta> gResponse = new GenericResponse<VMVenta>();
+
+            try 
+            {
+                //Usaurio para hacer prueba
+                modelo.IdUsuario = 1;
+
+                Venta venta_creada = await _ventaService.Registrar(_mapper.Map<Venta>(modelo));
+                modelo = _mapper.Map<VMVenta>(venta_creada);
+
+                gResponse.Estado = true;
+                gResponse.Objeto = modelo;
+            }
+            catch (Exception ex)
+            {
+                gResponse.Estado= false;
+                gResponse.Mensaje = ex.Message;
+            }
+
+            return StatusCode(StatusCodes.Status200OK, gResponse);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Historial(string numeroVenta, string fechaInicio, string fechaFin)
+        {
+            List<VMVenta> vmHistorialVenta = _mapper.Map<List<VMVenta>>(await _ventaService.Historial(numeroVenta, fechaInicio, fechaFin));
+
+            return StatusCode(StatusCodes.Status200OK, vmHistorialVenta);
         }
     }
 }
